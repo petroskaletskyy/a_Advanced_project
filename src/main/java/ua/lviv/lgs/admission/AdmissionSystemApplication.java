a2be7a7438f256b79c1d43dd1c00297d657e70b4
petro-skaletskyy <capone.petro@gmail.com>
2019-11-10 15:16:42 +0200

    create project

---
+++ src/main/java/ua/lviv/lgs/admission/AdmissionSystemApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs.admission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionSystemApplication.class, args);
+	}
+
+}
